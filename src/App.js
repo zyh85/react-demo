d28fe1a09d6ae177b2c130d601722824c7b123c5
web-swimming <791877206@qq.com>
2019-12-16 14:27:06 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, {useState} from 'react';
-// import InputNumber from './uncontrolInput/index.js';
-import InputNumber from './controlInput/index.js';
+import InputNumber from './Input/index.js';
 
-	function App(){
-      const [value,setValue] = useState('aaa')
-      return (
-          <div>
-          <InputNumber value={value} onChange={e=>{setValue(e)}}/>
-          <InputNumber defaultValue={value} onChange={e=>{setValue(e)}}/>
-          </div>
-      )
+function App(){
+    const [value,setValue] = useState('aaa')
+    let dValue = '1'
+    return (
+        <div>
+          <InputNumber value={value}  onChange={(e) => {setValue(e.target.value)}}/>
+          <InputNumber defaultValue={dValue} onChange={(e) => {
+              dValue = e.target.value
+          }}/>
+        </div>
+    )
+   
   }
   
 
