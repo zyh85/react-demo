5c4edef425bd6e5eb4ea82bd82f3737a116d7a82
web-swimming <791877206@qq.com>
2019-12-16 14:32:33 +0800

    Create index.js

---
+++ src/Input/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+class InputNumber extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			innerValue: ''
+		}
+	}
+	get isControl(){
+		return 'value' in this.props
+	}
+	get value() {
+		if(this.isControl){
+			return this.props.value
+		} else {
+			return this.state.innerValue
+		}
+	}
+	render() {
+		return (
+			<div>
+				<input
+					value={this.value}
+					onChange={(e) => {
+						if(!this.isControl){
+							this.setState({
+								innerValue: e.target.value
+							})
+						}
+						this.props.onChange(e)
+					}}
+				/>
+			</div>
+		)
+	}
+	componentDidMount() {
+		this.setState({
+			innerValue: this.props.defaultValue
+		})
+	}
+
+}
+  export default InputNumber
